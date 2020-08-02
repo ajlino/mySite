4943e1664d7945a85bfef4a07835cc048f8f782c
ajlino <38694859+ajlino@users.noreply.github.com>
2020-08-02 16:29:25 -0400

    Report Arrays!!!

--- createReport.php
+++ createReport.php
@@ -1,6 +1,71 @@
 <?php
-  session_start();
+
+include_once 'includes\php\timesheetDBH.php';
+$tableName="august2020";
+
+//Make an array ($fieldArray) of all of the column(field) names
+$sql = "SELECT * FROM $tableName";
+$result = mysqli_query($conn, $sql);
+$fieldArray = array();
+$count = mysqli_num_fields($result);
+for ($x=0; $x<$count; $x++){
+  $fieldName= (mysqli_fetch_field_direct($result, $x))->name;
+  array_push($fieldArray, $fieldName);
+}
+
+//Get array of unique names from schedule
+$sql = "SELECT * FROM $tableName";
+$result = mysqli_query($conn, $sql);
+$data = array();
+if (mysqli_num_rows($result) > 0) {
+ // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    foreach($row as $value){
+    array_push($data, $value);
+    }
+  }
+}
+//only include unique names in the array; get rid of duplicates
+$uniqueNames = array_unique($data);
+//sort alphabetically
+sort($uniqueNames);
+
+//create array starting with attending name, then all columns name appears in, then break
+foreach($uniqueNames as $name){
+  ${$name.'_array'} = array();  //name of array will be exp: $Linovitz_array
+  array_push(${$name.'_array'}, $name);
+  foreach($fieldArray as $field){
+    $sql = "SELECT $field FROM $tableName WHERE $field = '$name'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0){
+      array_push(${$name.'_array'}, $field);
+    }
+  }
+  // foreach(${'$name'.'array'} as $name_X){
+  //   echo $name_X." |";
+  // }
+  // echo ("<br />");
+}
+
+foreach ($Linovitz_array as $data){
+  echo $data." |";
+}
+echo "<br />";
+
+foreach ($Hang_array as $data){
+  echo $data." |";
+}
+
+
+
+
+mysqli_close($conn);
+
 ?>
+<!--
+
+
+
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -28,4 +93,4 @@
     <p>The Timesheet Robot</p>
 </div>
 
-<div class="container">
+<div class="container"> -->
